7a791a3c7d4d08882258dab2e1eced86afbde71e
B1N4L <163591915+B1N4L@users.noreply.github.com>
2024-10-17 17:38:10 +0530

    Update dashboard
    side bar button resopnsiveness  complete

--- busbuddy/src/pages/Dashboard/DashboardHome.jsx
+++ busbuddy/src/pages/Dashboard/DashboardHome.jsx
@@ -18,11 +18,12 @@ const Dashboard = () => {
       <ul className="flex-column space-y-4 text-sm font-medium text-gray-500 md:me-4 mb-4 md:mb-0">
         <li>
           <Link to="profile" 
-            className="inline-flex items-center px-4 py-3 text-white bg-orange-600 rounded-lg active w-full" aria-current="page"
+            className={`${activeTab === "profile" ? "inline-flex items-center px-4 py-3 text-white bg-orange-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("profile")} // Add click handler here
           >
             <svg 
-              className="w-4 h-4 me-2 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              //className={`${activeTab === "profile" ? "" : "w-4 h-4 me-2 text-white"}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"
+                className={`${activeTab === "profile" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
               <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
             </svg>
             Profile
@@ -43,11 +44,11 @@ const Dashboard = () => {
         </li>
         <li>
           <Link to="delete" 
-            className={`${activeTab === "manage" ? "inline-flex items-center px-4 py-3 text-white bg-orange-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
+            className={`${activeTab === "delete" ? "inline-flex items-center px-4 py-3 text-white bg-orange-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("delete")}
           >
             <svg 
-              className="w-4 h-4 me-2 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              className={`${activeTab === "delete" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
               <path d="M18 7.5h-.423l-.452-1.09.3-.3a1.5 1.5 0 0 0 0-2.121L16.01 2.575a1.5 1.5 0 0 0-2.121 0l-.3.3-1.089-.452V2A1.5 1.5 0 0 0 11 .5H9A1.5 1.5 0 0 0 7.5 2v.423l-1.09.452-.3-.3a1.5 1.5 0 0 0-2.121 0L2.576 3.99a1.5 1.5 0 0 0 0 2.121l.3.3L2.423 7.5H2A1.5 1.5 0 0 0 .5 9v2A1.5 1.5 0 0 0 2 12.5h.423l.452 1.09-.3.3a1.5 1.5 0 0 0 0 2.121l1.415 1.413a1.5 1.5 0 0 0 2.121 0l.3-.3 1.09.452V18A1.5 1.5 0 0 0 9 19.5h2a1.5 1.5 0 0 0 1.5-1.5v-.423l1.09-.452.3.3a1.5 1.5 0 0 0 2.121 0l1.415-1.414a1.5 1.5 0 0 0 0-2.121l-.3-.3.452-1.09H18a1.5 1.5 0 0 0 1.5-1.5V9A1.5 1.5 0 0 0 18 7.5Zm-8 6a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7Z"/>
             </svg>
             Delete
@@ -55,11 +56,11 @@ const Dashboard = () => {
         </li>
         <li>
           <Link to="info" 
-            className={`${activeTab === "manage" ? "inline-flex items-center px-4 py-3 text-white bg-orange-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
+            className={`${activeTab === "info" ? "inline-flex items-center px-4 py-3 text-white bg-orange-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("info")}
           >
             <svg 
-              className="w-4 h-4 me-2 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              className={`${activeTab === "info" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
               <path d="M7.824 5.937a1 1 0 0 0 .726-.312 2.042 2.042 0 0 1 2.835-.065 1 1 0 0 0 1.388-1.441 3.994 3.994 0 0 0-5.674.13 1 1 0 0 0 .725 1.688Z"/>
               <path d="M17 7A7 7 0 1 0 3 7a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h1a1 1 0 0 0 1-1V7a5 5 0 1 1 10 0v7.083A2.92 2.92 0 0 1 12.083 17H12a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h3a4.92 4.92 0 0 0 4.917-4.917V10a3 3 0 0 0-3-3Z"/>
             </svg>
@@ -68,11 +69,11 @@ const Dashboard = () => {
         </li>
         <li>
           <Link to="prices" 
-            className={`${activeTab === "manage" ? "inline-flex items-center px-4 py-3 text-white bg-orange-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
+            className={`${activeTab === "prices" ? "inline-flex items-center px-4 py-3 text-white bg-orange-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("prices")}
           >
             <svg 
-              className="w-4 h-4 me-2 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              className={`${activeTab === "prices" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path d="M7 7h10M7 12h10M7 17h10"/>
             </svg>
             Prices
@@ -80,11 +81,11 @@ const Dashboard = () => {
         </li>
         <li>
           <Link to="timetable" 
-            className="inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"
+            className={`${activeTab === "timetable" ? "inline-flex items-center px-4 py-3 text-white bg-orange-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("timetable")}
           >
             <svg 
-              className="w-4 h-4 me-2 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              className={`${activeTab === "timetable" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path d="M5 12h14M12 5v14"/>
             </svg>
             Timetable
@@ -92,11 +93,11 @@ const Dashboard = () => {
         </li>
         <li>
           <Link to="routes" 
-            className="inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"
+            className={`${activeTab === "routes" ? "inline-flex items-center px-4 py-3 text-white bg-orange-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("routes")}
           >
             <svg 
-              className="w-4 h-4 me-2 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              className={`${activeTab === "routes" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path d="M12 2l8 8-8 8-8-8 8-8z"/>
             </svg>
             Routes
