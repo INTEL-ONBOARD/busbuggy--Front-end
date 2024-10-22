530408c1768d83b6912b75e466a9f2780923f466
B1N4L <163591915+B1N4L@users.noreply.github.com>
2024-10-22 15:18:38 +0530

    Update: logout

--- busbuddy/src/components/Navbar.jsx
+++ busbuddy/src/components/Navbar.jsx
@@ -2,10 +2,12 @@ import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png'; // Import the logo image
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import Modal from 'react-modal';
 
 function Navbar() {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false); // state to check user info from localStorage
+  const [isLogoutConfModalOpen, SetIsLogoutConfModalOpen] = useState(false); //state for logout confirmation modal
 
   // Check if user data exists in localStorage when the component mounts
   useEffect(() => {
@@ -21,6 +23,7 @@ function Navbar() {
   const handleLogout = () => {
     localStorage.removeItem('userData'); // Remove user data from localStorage
     setIsLoggedIn(false); // Update the state to logged out
+    SetIsLogoutConfModalOpen(false);
     navigate('/');
   };
 
@@ -60,7 +63,7 @@ function Navbar() {
             <div className="flex md:ml-auto">
               <button 
                 className="my-2 inline-flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-orange-400 bg-white rounded-md border border-white hover:bg-transparent hover:border-white hover:text-white focus:bg-transparent focus:text-white md:mx-2 md:my-0"
-                onClick={handleLogout}
+                onClick={() => SetIsLogoutConfModalOpen(true)}
               >
                 Logout
               </button>
@@ -77,7 +80,47 @@ function Navbar() {
           )}
         </div>
       </div>
+
+      {/*logout confirmation modal*/}
+      <Modal 
+        isOpen={isLogoutConfModalOpen}
+        onRequestClose={() => SetIsLogoutConfModalOpen(false)}
+        contentLabel="Logout Confirmation"
+        className="flex rounded w-3/4 mx-auto mt-20 flex-col justify-center items-center"
+      >
+        <h2 className="m-6 text-orange-500 text-3xl">Logout Confirmation</h2>
+        <div className="bg-white border border-gray-300 border-b-2 shadow-sm p-8 w-1/3 rounded-md">
+        <div className="text-center">
+        <i className="fi fi-rs-triangle-warning text-9xl text-orange-400"></i>
+        </div>
+          <h1 className="text-center text-orange-400 text-3xl">Warning</h1>
+          <p className="text-black m-6 rounded-md p-6 text-lg">
+            Are you sure you want to Logout from your Account?
+          </p>
+
+          <div className="felx flex-row text-center gap-6 justify-around">
+            <button 
+              type="button" 
+              className="h-10 px-5 py-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 transition duration-300 mr-8"
+              onClick={handleLogout}
+              >
+              Yes
+            </button>
+            <button 
+              type="button" 
+              className="w-20 mt-3 h-10 px-4 py-2 m-1 text-gray-600 transition-colors duration-300 transform bg-white rounded-md border border-gray-400 hover:text-black hover:bg-gray-100 hover:border-gray-600 focus:outline-none"
+              onClick={() => SetIsLogoutConfModalOpen(false)}
+              >
+                No
+            </button>
+
+          </div>
+      </div>
+
+
+      </Modal>
     </nav>
+
   );
 }
 
